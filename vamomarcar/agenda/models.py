25f149c16dcfb5e4ea42efb177382a264ff31683
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-02-06 11:04:09 -0300

    Cria modelos (ORM)

--- vamomarcar/agenda/models.py
+++ vamomarcar/agenda/models.py
@@ -1,15 +1,19 @@
-class Evento:
+from django.db import models
+
+class Evento(models.Model):
+    # Primary Key (pk) é criada automaticamente
+    nome = models.CharField(max_length=256)
+    categoria = models.ForeignKey("Categoria", on_delete=models.SET_NULL, null=True)
+    local = models.CharField(blank=True, max_length=256)
+    link = models.URLField(blank=True, max_length=256)
+
+
+class Categoria(models.Model):
     CATEGORIAS = [
-        "Backend",
-        "Frontend",
-        "Fullstack",
-        "UX/UI",
+        ("Backend", "Backend"),
+        ("Frontend", "Frontend"),
+        ("Fullstack", "Fullstack"),
+        ("UX/UI", "UX/UI"),
     ]
 
-    def __init__(self, nome, categoria, local=None, link=None):
-        if not categoria in Evento.CATEGORIAS:
-            raise ValueError(f"Categoria {categoria} inexistente. Opções: {Evento.CATEGORIAS}")
-        self.nome = nome
-        self.categoria = categoria
-        self.local = local
-        self.link = link
+    nome = models.CharField(unique=True, max_length=256, choices=CATEGORIAS)
--- vamomarcar/agenda/views.py
+++ vamomarcar/agenda/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from agenda.models import Evento
 
 
-aula_python = Evento(nome="Aula de Python", categoria="Backend", local="Rio de Janeiro")
-aula_js = Evento(nome="Aula de JavaScript", categoria="Fullstack", link="http://tamarcado.com/js")
+aula_python = dict(nome="Aula de Python", categoria="Backend", local="Rio de Janeiro")
+aula_js = dict(nome="Aula de JavaScript", categoria="Fullstack", link="http://tamarcado.com/js")
 eventos = [
     aula_python,
     aula_js,
