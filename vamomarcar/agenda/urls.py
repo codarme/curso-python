cc52c625db282c292f586d381a122aac777b4d39
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-02-09 09:14:30 -0300

    Adiciona navegação entre detalhes e listagem de eventos

--- vamomarcar/agenda/urls.py
+++ vamomarcar/agenda/urls.py
@@ -3,6 +3,6 @@ from .views import exibir_evento, listar_eventos
 
 
 urlpatterns = [
-    path('eventos/<int:id>/', exibir_evento),
-    path('eventos/', listar_eventos),
+    path('eventos/<int:id>/', exibir_evento, name='exibir_evento'),
+    path('eventos/', listar_eventos, name='listar_eventos'),
 ]
