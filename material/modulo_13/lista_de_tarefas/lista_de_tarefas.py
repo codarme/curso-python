d0b3168f6cbb398de8a1ed041cdc6d088a36a45f
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-02-25 09:01:51 -0300

    Extrai modelos para arquivos

--- material/modulo_13/lista_de_tarefas/lista_de_tarefas.py
+++ material/modulo_13/lista_de_tarefas/lista_de_tarefas.py
@@ -1,37 +1,3 @@
-class Tarefa:
-    def __init__(self, titulo, descricao="", data=None, notificacao=None, concluida=False):
-        self.titulo = titulo
-        self.descricao = descricao
-        self.data = data
-        self.notificacao = notificacao
-        self.concluida = concluida
-
-    def concluir(self):
-        """
-        Define essa tarefa como concluida.
-        """
-        pass
-
-    def adicionar_descricao(self, descricao):
-        """
-        Adiciona uma descrição para a tarefa.
-        """
-        pass
-
-    def adiar_notificacao(self, minutos):
-        """
-        Adia a notificação em uma certa quantidade de minutos.
-        """
-        pass
-
-    def atrasada(self):
-        """
-        Diz se tarefa está atrasada. Ou seja, data < hoje.
-        """
-        pass
-
-
-
 class ListaDeTarefas:
     def __init__(self):
         """
