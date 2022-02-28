cdbbec4479358aad78b07eb8edae467dff8970a7
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-02-28 20:50:35 -0300

    Adiciona exemplo do Django com testes

--- vamomarcar/agenda/tests.py
+++ vamomarcar/agenda/tests.py
@@ -1,34 +1,50 @@
-from datetime import date, timedelta
+from datetime import date
 from django.test import TestCase, Client
 
-from agenda.models import Evento
+from agenda.models import Evento, Categoria
 
 
-class TestListarEventos(TestCase):
-    """
-    Test cases:
-    - Se não houver nenhum evento criado, exibe lista vazia (nenhum <tr>)
-    - Se houverem apenas eventos criados no passado, exibe lista vazia
-        - Ou passa para o contexto um elemento: {"eventos": []} (len == 0)
-    - Se houverem eventos criados no futuro, passa para o contexto esses eventos
-        - Ou, exibir HTML (mais "chato" porque vamos precisar ficar atualizando nosso teste)
-        - Boa prática: não testar constantes, testar comportamentos
-    - Utiliza o template correto
-    - Retorna 200 caso tudo ocorra bem
-    """
-    def test_lista_eventos_com_a_data_do_dia_atual(self):
-        evento = Evento.objects.create(
-            nome="Evento Hoje",
-            data=date.today(),
-        )
-        response = Client().get("/eventos/")
-        self.assertIn("Evento Hoje", response.content.decode())
+class TestPaginaInicial(TestCase):
+    def test_lista_eventos(self):
+        client = Client()
+        response = client.get("/")
+        # print(response.content)
+        # self.assertContains(response, "<th>Nome</th>")
+        self.assertTemplateUsed(response, "agenda/listar_eventos.html")
 
-    def test_nao_exibe_eventos_passados(self):
-        evento = Evento.objects.create(
-            nome="Evento Hoje",
-            data=date.today() - timedelta(days=1),  # Ontem
-        )
-        response = Client().get("/eventos/")
-        self.assertNotIn("Evento Hoje", response.content.decode())
-        self.assertEqual(len(response.context["eventos"]), 0)
+
+class TestListagemDeEventos(TestCase):
+    def test_evento_com_data_de_hoje_e_exibido(self):
+        categoria = Categoria()
+        categoria.name = "Backend"
+        categoria.save()
+
+        evento = Evento()
+        evento.nome = "Aula de Python"
+        evento.categoria = categoria
+        evento.local = "Rio de Janeiro"
+        evento.data = date.today()
+        evento.save()
+
+        client = Client()
+        response = client.get("/")
+        self.assertContains(response, "Aula de Python")
+        self.assertEqual(list(response.context["eventos"]), [evento])
+
+    def test_eventos_sem_data_sao_exibidos(self):
+        categoria = Categoria()
+        categoria.name = "Backend"
+        categoria.save()
+
+        evento = Evento()
+        evento.nome = "Aula de Python"
+        evento.categoria = categoria
+        evento.local = "Rio de Janeiro"
+        evento.data = None
+        evento.save()
+
+        client = Client()
+        response = client.get("/")
+        self.assertContains(response, "Aula de Python")
+        self.assertContains(response, "A definir")
+        self.assertEqual(list(response.context["eventos"]), [evento])
