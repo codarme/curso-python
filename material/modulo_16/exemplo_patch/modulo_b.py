aca7eb8d47b614c0a81422f7c8c2ee6bfd4dd2cf
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-03-14 16:44:03 -0300

    Adiciona exemplo de onde fazer o patch

---
+++ material/modulo_16/exemplo_patch/modulo_b.py
@@ -0,0 +1,3 @@
+def foo_modulo_b():
+    print("modulo_b.py")
+    return "foo_modulo_b"
---
+++ material/modulo_16/exemplo_patch/modulo_c.py
@@ -0,0 +1,5 @@
+from exemplo_patch import modulo_b
+
+def foo_modulo_c():
+    print("modulo_c.py")
+    return modulo_b.foo_modulo_b()
