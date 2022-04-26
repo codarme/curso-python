453c5c14064edeec90821f952d626f38017f803b
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2022-04-25 22:26:00 -0300

    Adiciona slides

--- lives/algoritmos/lista_vs_conjunto.py
+++ lives/algoritmos/lista_vs_conjunto.py
@@ -2,7 +2,7 @@
 Executar os comandos abaixo no terminal!
 
 python -m timeit --setup "import random;lista = list(range(1000))" "random.randint(1, 100000) in lista"
-python -m timeit --setup "import random;lista = set(range(1000))" "random.randint(1, 100000) in lista"
+python -m timeit --setup "import random;conjunto = set(range(1000))" "random.randint(1, 100000) in conjunto"
 
 Depois aumentar o range(1000) => range(1000000) e verificar a diferença.
 """
